16596294e29ae4334cbf65906ae397279acdcda1
eviterin <ethan.viterin@gmail.com>
2024-01-27 22:43:48 +0100

    Added custom tailwind box shadow

--- packages/webapp/tailwind.config.cjs
+++ packages/webapp/tailwind.config.cjs
@@ -2,7 +2,14 @@
 const config = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      // Custom box shadow that adds a 'highlight' effect 
+      // For example, add 'shadow-highlight shadow-orange-300' to className
+      // See: https://tailwindcss.com/docs/box-shadow#customizing-your-theme
+      boxShadow: {
+        'highlight': '0 0 20px', 
+      }
+    },
   },
   plugins: [require("daisyui")],
   daisyui: {

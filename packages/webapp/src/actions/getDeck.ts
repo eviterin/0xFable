03fcd6d745b8fada2d1573ba0c2cf9189fb412fd
eviterin <ethan.viterin@gmail.com>
2024-02-01 20:22:25 +0100

    Added action classes to get and set decks on chain

---
+++ packages/webapp/src/actions/getDeck.ts
@@ -0,0 +1,53 @@
+import { defaultErrorHandling } from "src/actions/errors"
+import { contractWriteThrowing } from "src/actions/libContractWrite"
+import { Address } from "src/chain"
+import { deployment } from "src/deployment"
+import { inventoryABI } from "src/generated"
+import { checkFresh, freshWrap } from "src/store/checkFresh"
+import { getOpponentIndex } from "src/store/read"
+
+// =================================================================================================
+
+export type getAllDecksArgs = {
+  playerAddress: Address
+  setLoading: (label: string | null) => void
+  onSuccess: () => void
+}
+
+// -------------------------------------------------------------------------------------------------
+
+/**
+ * Fetches all decks of the given player by sending the `getAllDecks` transaction.
+ *
+ * Returns `true` iff the transaction is succesful.
+ */
+export async function getAllDecks(args: getAllDecksArgs): Promise<boolean> {
+  try {
+    return await getAllDecksImpl(args)
+  } catch (err) {
+    args.setLoading(null)
+    return defaultErrorHandling("getAllDecks", err)
+  }
+}
+
+
+// -------------------------------------------------------------------------------------------------
+
+async function getAllDecksImpl(args: getAllDecksArgs): Promise<any> {
+  const decks = await contractWriteThrowing({
+    contract: deployment.Inventory,
+    abi: inventoryABI,
+    functionName: "getAllDecks",
+    args: [
+      args.playerAddress,
+    ],
+    setLoading: args.setLoading
+  })
+
+  checkFresh(await freshWrap(decks))
+
+  args.onSuccess()
+  return decks
+}
+
+// =================================================================================================

769f3790e2247e79a7229c166936fb02a26a2777
eviterin <ethan.viterin@gmail.com>
2024-02-06 10:57:03 +0100

    disabled deck buttons while loading deck from chain

--- packages/webapp/src/components/collection/deckList.tsx
+++ packages/webapp/src/components/collection/deckList.tsx
@@ -33,11 +33,13 @@ const DeckCollectionDisplay: React.FC<DeckCollectionDisplayProps> = ({ decks, on
           <button 
             key={deckID} 
             onClick={() => onDeckSelect(deckID)}
-            className="w-full px-4 py-2 mb-2 border rounded-md text-gray-100 bg-purple-500 hover:bg-gray-500 font-bold text-center focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            className={`w-full px-4 py-2 mb-2 border rounded-md text-gray-100 ${isLoadingDecks ? 'bg-gray-400 cursor-not-allowed' : 'bg-purple-500 hover:bg-gray-500'} font-bold text-center focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent`}
+            disabled={isLoadingDecks}
           >
             {deck.name}
           </button>
         ))}
+
       </div>
   )
 }
